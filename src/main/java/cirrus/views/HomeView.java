2141bb086b429ecc097cd60171fa72e334fa167b
Paul Kim <Paul Kim>
2016-12-09 23:56:17 -0800

    Home View with Project folder icons.

--- src/main/java/cirrus/views/HomeView.java
+++ src/main/java/cirrus/views/HomeView.java
@@ -1,6 +1,8 @@
 
 package cirrus.views;
 
+import javax.swing.GroupLayout.Alignment;
+
 import org.vaadin.spring.sidebar.annotation.FontAwesomeIcon;
 import org.vaadin.spring.sidebar.annotation.SideBarItem;
 
@@ -8,6 +10,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Sizeable;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringView;
@@ -18,6 +21,7 @@ import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.InlineDateField;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.BaseTheme;
 import com.vaadin.ui.themes.ValoTheme;
@@ -39,47 +43,46 @@ import cirrus.Sections;
 public class HomeView extends VerticalLayout implements View {
 
     public HomeView() {// @import "HomeTheme.scss";
-        setSpacing(true);
+    	setSizeFull();
         setMargin(true);
+        
+        Panel panel = new Panel();
+        CssLayout layout = new CssLayout();
+        layout.setSizeFull();
 
         Label header = new Label("Welcome to Cirrus Drive!");
         header.addStyleName(ValoTheme.LABEL_H1);
+        //header.setSizeFull();
+        //header.setSizeUndefined();
         addComponent(header);
 
         Label body = new Label("<p>This is the body. Formatted with html.</p>");
         body.setContentMode(ContentMode.HTML);
+        //body.setSizeFull();
+        //body.setSizeUndefined();
         addComponent(body);
-        
-        // Create a 4 by 4 grid layout.
-        //CssLayout grid = new CssLayout();
-        HorizontalLayout grid = new HorizontalLayout();
-        MarginInfo margin = new MarginInfo(10);
-        margin.setMargins(true);
-        //grid.setMargin(true);
-        grid.setMargin(margin);
-        grid.setWidth("100%");
-        grid.setSpacing(false);
-        
-        grid.setWidthUndefined();
-        grid.addStyleName("Example Horiztonal Wrap");
 
-        // Fill out the first row using the cursor.
-        for (int i = 0; i < 20; i++) {
-        	Button b = new Button("Col " + (grid.getComponentCount() + 1));
-        	b.setIcon(FontAwesome.FOLDER);
-        	b.setWidth("75px");
-        	b.setHeight("75px");
-        	b.addStyleName(BaseTheme.BUTTON_LINK);
-            grid.addComponent(b);
+        // Create three equally expanding components.
+        for (int i = 1; i <= 200; i++) {
+            Button button = new Button("Col " + (layout.getComponentCount() + 1));
+            button.setIcon(FontAwesome.FOLDER);
+        	//b.setWidth("75px");
+        	//b.setHeight("75px");
+            button.addStyleName(BaseTheme.BUTTON_LINK);
+            //button.setWidth("75px");
+            //button.setHeight("75px");
+            button.setWidth("10%");
+            //button.setHeight("75%");
+            layout.addComponent(button);
+            
+            // Have uniform 1:1:1 expand ratio.
+            //layout.setExpandRatio(button, 1.0f);
         }
-
         
-        grid.addComponent(new Label("1x2 cell"));
-        InlineDateField date = new InlineDateField("A 2x2 date field");
-        date.setResolution(DateField.RESOLUTION_DAY);
-        grid.addComponent(date);
-        
-        addComponent(grid);
+        panel.setSizeFull();
+        panel.setContent(layout);
+        panel.getContent().setSizeUndefined();
+        addComponent(panel);
     }
 
     @Override
