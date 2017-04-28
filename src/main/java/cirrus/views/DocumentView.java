316e1608a9c9f4043cb2217fe42bed05713b20fd
Aleks Kivuls <alekskivuls@users.noreply.github.com>
2017-04-27 19:54:44 -0700

    Fix broken build by adding back build to doc view

--- src/main/java/cirrus/views/DocumentView.java
+++ src/main/java/cirrus/views/DocumentView.java
@@ -17,6 +17,7 @@ import com.vaadin.ui.VerticalLayout;
 
 import cirrus.Sections;
 import cirrus.backend.DocumentBackend;
+import cirrus.docker.Language;
 import cirrus.models.Document;
 import cirrus.templates.documents.BuildMenuBar;
 import cirrus.templates.documents.DocumentPanel;
@@ -90,9 +91,20 @@ public class DocumentView extends VerticalLayout implements View {
 		}
 	}
 
+	public void buildProgram() {
+		Language lang = Language.fromExtension(getCurrFileExtension());
+		if (lang != null) {
+			String result = mBackend.buildProgram(doc.getDocBody(), lang);
+			documentTabs.appendConsole(result);
+		}
+	}
+
 	public void runProgram() {
-		String result = mBackend.runProgram(doc.getDocBody());
-		documentTabs.appendConsole(result);
+		Language lang = Language.fromExtension(getCurrFileExtension());
+		if (lang != null) {
+			String result = mBackend.runProgram(doc.getDocBody(), lang);
+			documentTabs.appendConsole(result);
+		}
 	}
 
 	public void save() {
@@ -111,18 +123,25 @@ public class DocumentView extends VerticalLayout implements View {
 		try {
 			UI.getCurrent().addWindow(prefWindow);
 		} catch (Exception e) {
-			//Window is already visible
+			// Window is already visible
 		}
 	}
-	
+
 	public void tabToIndent() {
-		StringBuilder sb = new StringBuilder("textArea.setAttribute('spellcheck','false'); var textarea = document.querySelector('.v-textarea');");
+		StringBuilder sb = new StringBuilder(
+				"textArea.setAttribute('spellcheck','false'); var textarea = document.querySelector('.v-textarea');");
 		sb.append("textarea.addEventListener('keydown', function(e) {")
-			.append("if(e.key == 'Tab') { e.preventDefault(); var s = this.selectionStart;")
-			.append("this.value = this.value.substring(0, this.selectionStart) + '\t' + this.value.substring(this.selectionEnd);")
-			.append("this.selectionEnd = s + 1;}});");
-								
-		String script = sb.toString();	
+				.append("if(e.key == 'Tab') { e.preventDefault(); var s = this.selectionStart;")
+				.append("this.value = this.value.substring(0, this.selectionStart) + '\t' + this.value.substring(this.selectionEnd);")
+				.append("this.selectionEnd = s + 1;}});");
+
+		String script = sb.toString();
 		JavaScript.getCurrent().execute(script);
+	}
+
+	private String getCurrFileExtension() {
+		String docName = doc.getDocName();
+		int extIndex = docName.lastIndexOf('.');
+		return extIndex != -1 ? docName.substring(extIndex + 1) : "";
 	}
 }
